05dace8e217a748eefed9acbc1c05ca85571bf19
Andrea Ceci <andrea@MacBook-Pro-di-Andrea.local>
2022-09-09 00:53:03 +0200

    Implementato controllo su diffenrenze mese degli anni richiesti

--- esame.py
+++ esame.py
@@ -15,9 +15,11 @@ class CSVTimeSeriesFile:
             file = open(self.name,'r')
             file.readline()
 
+        # Se ho errori nell' apertura dei file alzo eccezione
         except:
             raise ExamException("Errore nell'apertura del file")
 
+        # Inizializzo lista vuota
         data = []
 
         for item in file:
@@ -26,7 +28,7 @@ class CSVTimeSeriesFile:
 
             element[-1] = element[-1].strip()
             
-            # Controllo Conversione a intero e non negativo
+            # Controllo Conversione a intero e se i valori sono ammissibili
             try:
                 app = element[0].split('-')
 
@@ -34,48 +36,59 @@ class CSVTimeSeriesFile:
                 test_int_2 = int(app[1])
                 test_int_3 = int(element[1])
 
-                if test_int_1 and test_int_2 and test_int_3 > 0:
+                # Se i valori presenti nel file sono ammissibili li aggiungo alla lista creata precedentemente
+                if test_int_1 and test_int_2 and test_int_3 > 0 and test_int_1 > 0 and test_int_2 > 0 and test_int_2 <= 12:
                     data.append([element[0], element[1]])
 
             except:
-                #print("Errore nella conversione a intero")
                 pass
 
+        # Chiudo il file
         file.close()
 
+        # Controllo che la lista non sia vuota altrimenti alzo eccezione
         if len(data) == 0:
             raise ExamException('Lista vuota')
 
+        # Controllo che non sono presenti TimeStamp duplicati o non ordinati altrimenti alzo eccezione
         for i in range(len(data)-1):
             
             if data[i][0] >= data[i+1][0]:
                 raise ExamException("TimeStamp non ordinato o duplicato")
 
+        # Ritorno la lista 
         return data
 
+# Dichiaro funzione che occorre per calcolare la differenza tra gli anni inseriti dall'utente
 def detect_similar_monthly_variations(time_series, years):
     
+    # Credo una lista vuota di appoggio
     list_app = []
 
+    # Splitto elementi presenti nella time_series sul carattere "-"
     for item in time_series:
         app = item[0].split('-')
 
+        # Inserisco gli anni presenti nei dati della time_series nella lista di appoggio
         list_app.append(int(app[0]))
 
     # Uso SET, collezione non ordinata e unica di elementi
     list_app = list(set(list_app))
 
-    # Controllo se anni presenti nel dataset
+    # Controllo se anni presenti nel dataset altrimenti alzo eccezione
     for item in years:
         if item not in list_app:
             raise ExamException("Anno cercato non presente nel dataset")
 
+    # Controllo che gli anni forniti dall'utente siano consecutivi
     if years[0]+1 != years[1]:
         raise ExamException("Anni forniti non consecutivi")
 
+    # Istanzio due liste vuote per inserire i valori degli anni richiesti dall'utente
     listyear0 = []
     listyear1 = []
 
+    # Itero sulla time_series per estrarre i dati richiesti dall'utente assegnandoli alla lista specifica dedicata
     for item in time_series:
 
         year_app = int((item[0].split('-'))[0])
@@ -88,46 +101,156 @@ def detect_similar_monthly_variations(time_series, years):
         if int(year_app) == years[1]:
             listyear1.append([year_app, day_app, num_app])
     
+    # Istanzio due liste vuote che conterranno le differenze tra i diversi mesi per i due anni richiesti dall'utente
     diff_list0 = []
     diff_list1 = []
 
-    i = 0
-    j = 0
+    # Assegno a variabile la lunghezza della lista estratta
+    lung = len(listyear0)
 
-    while i < 10:
+    # Creo due variabili per controllare se manca il primo o l'ultimo elementi
+    missingfirst = False
+    missinglast = False
+
+    # Istanzio indice i
+    i = 0
+
+    # Controllo se il primo elemento è diverso da Gennaio
+    if listyear0[0][1] != 1:
+        missingfirst = True
+    #print('***** MissingFirst -->', missingfirst)   
+
+    # Se diverso da Gennaio aggiungo in cima alla lista dei "None" fino ad arrivare al mese con cui inizia l'anno
+    if missingfirst == True:
+        while i+1 < listyear0[0][1]:
+            diff_list0.append(None)
+            i += 1
+    
+    # Reimposto indice i
+    i = 0
+
+    #Ciclo per tutta la lunghezza della lista
+    while i < lung-1:
 
         #print(" ")
-        #print('i:', i)
-        #print('mese corr:', ((listyear0[i][1])), '- mese succ:', listyear0[i+1][1] )
-        #print('pass corr:', ((listyear0[i][2])), '- pass succ:', listyear0[i+1][2] )
 
-        if (listyear0[i][1])+1 == (listyear0[i+1][1]):
-            #print('Consecutivo')
+        # Se aggiungendo 1 al mese corrente raggiungo il mese successivo aggiungo alla lista il valore assoluto della differenza
+        #print("mese corr", listyear0[i][1], "mese succ:",listyear0[i+1][1])
+        if listyear0[i][1]+1 == listyear0[i+1][1]:
 
-            app = abs((listyear0[i][2]) -  listyear0[i+1][2])
-
+            app = abs(listyear0[i][2] - listyear0[i+1][2])
             diff_list0.append(app)
-            i += 1
-
+        
+        # Altrimenti aggiungo il valore "None" finchè non raggiungo il successivo elemento della lista
         else:
-            #print(" ")
-            #print('Non consecutivo')   
-            j = (listyear0[i][1])
+            #print("Diverso")
+            j = listyear0[i][1]
 
-            while j != (listyear0[i+1][1]):
-                #print("j:", j, '- mese targhet', listyear0[i+1][1])
-                #print('Append None')
+            while j < listyear0[i+1][1]:
+
+                #print("j:", j, "confronto con:",listyear0[i+1][1])
+
                 diff_list0.append(None)
+
                 j += 1
 
+        i += 1
+
+    # Controllo se ultimo elemento mese della lista corrisponde a dicembre
+
+    if listyear0[len(listyear0)-1][1] != 12:
+        missinglast = True
+    #print('***** MissingLast --> ', missinglast)
+
+    # Se ultimo mese della lista non corrisponde a dicembre aggiungo valore "None" alla lista finchè indice non raggiunge 12
+    # completando cosi tutti i confronti 
+    if missinglast == True:
+
+        i = listyear0[lung-1][1]
+        while i < 12:
+            diff_list0.append(None)
             i += 1
 
-    print('Lunghezza', len(diff_list0)) 
+    #print(" ")
+    #print("Lunghezza lista:", len(diff_list0))
+    #for item in diff_list0:
+        #print(item)
+        
 
-    for item in diff_list0:
-        print(item)
-        pass
+# CONTROLLO VALORI SECONDO ANNO
 
+    lung = len(listyear1)
+    #print("lung", lung)
+
+    # Creo due variabili per controllare se manca il primo o l'ultimo elementi
+    missingfirst = False
+    missinglast = False
+
+    # Istanzio indice i
+    i = 0
+
+    # Controllo se il primo elemento è diverso da Gennaio
+    if listyear1[0][1] != 1:
+        missingfirst = True
+    #print('***** MissingFirst -->', missingfirst)   
+
+    # Se diverso da Gennaio aggiungo in cima alla lista dei "None" fino ad arrivare al mese con cui inizia l'anno
+    if missingfirst == True:
+        while i+1 < listyear1[0][1]:
+            diff_list1.append(None)
+            i += 1
+    
+    # Reimposto indice i
+    i = 0
+
+    #Ciclo per tutta la lunghezza della lista
+    while i < lung-1:
+
+        #print(" ")
+
+        # Se aggiungendo 1 al mese corrente raggiungo il mese successivo aggiungo alla lista il valore assoluto della differenza
+        #print("mese corr", listyear1[i][1], "mese succ:",listyear1[i+1][1])
+        #print("pass corr", listyear1[i][2], "pass succ:",listyear1[i+1][2])
+        if listyear1[i][1]+1 == listyear1[i+1][1]:
+
+            app = abs(listyear1[i][2] - listyear1[i+1][2])
+            diff_list1.append(app)
+        
+        # Altrimenti aggiungo il valore "None" finchè non raggiungo il successivo elemento della lista
+        else:
+            #print("Diverso")
+            j = listyear1[i][1]
+
+            while j < listyear1[i+1][1]:
+
+                #print("j:", j, "confronto con:",listyear1[i+1][1])
+
+                diff_list1.append(None)
+
+                j += 1
+
+        i += 1
+
+    # Controllo se ultimo elemento mese della lista corrisponde a dicembre
+
+    if listyear1[len(listyear1)-1][1] != 12:
+        missinglast = True
+    #print('***** MissingLast --> ', missinglast)
+
+    # Se ultimo mese della lista non corrisponde a dicembre aggiungo valore "None" alla lista finchè indice non raggiunge 12
+    # completando cosi tutti i confronti 
+    if missinglast == True:
+
+        i = listyear1[lung-1][1]
+        while i < 12:
+            diff_list1.append(None)
+            i += 1
+
+    #print(" ")
+    #print("Lunghezza lista:", len(diff_list1))
+
+    #for item in diff_list1:
+    #    print(item)
 
 
 #==============================
